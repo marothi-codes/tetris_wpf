1aa56a08c41f9cef8c412910960ef15ae17fd942
Marothi Mahlake <marothi.codes@gmail.com>
2022-02-05 11:39:23 +0200

    TBlock class now implemented.

--- Tetris/ViewModels/TBlock.cs
+++ Tetris/ViewModels/TBlock.cs
@@ -1,6 +1,19 @@
-﻿namespace Tetris.ViewModels
+﻿using Tetris.Infrastructure;
+
+namespace Tetris.ViewModels
 {
-	public class TBlock
+	public class TBlock : Block
 	{
+		private readonly Position[][] tiles = new Position[][]
+		{
+			new Position[] { new(0,1), new(1,0), new(1,1), new(1,2) },
+			new Position[] { new(0,1), new(1,1), new(1,2), new(2,1) },
+			new Position[] { new(1,0), new(1,1), new(1,2), new(2,1) },
+			new Position[] { new(0,1), new(1,0), new(1,1), new(2,1) }
+		};
+
+		public override int Id => 6;
+		protected override Position StartOffset => new(0, 3);
+		protected override Position[][] Tiles => tiles;
 	}
 }
