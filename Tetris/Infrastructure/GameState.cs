991a1bb88e95387a3f968a439ff269767ac05659
Marothi Mahlake <marothi.codes@gmail.com>
2022-02-09 11:20:37 +0200

    Block Rotation and Moving methods implemented.

--- Tetris/Infrastructure/GameState.cs
+++ Tetris/Infrastructure/GameState.cs
@@ -1,6 +1,80 @@
 ﻿namespace Tetris.Infrastructure
 {
-	class GameState
+	public class GameState
 	{
+		private Block currentBlock;
+
+		public Block CurrentBlock
+		{
+			get => currentBlock;
+			private set
+			{
+				currentBlock = value;
+				currentBlock.Reset();
+			}
+		}
+
+		public GameGrid GameGrid { get; }
+		public BlockStore BlockStore { get; }
+		public bool GameOver { get; private set; }
+
+		public GameState()
+		{
+			GameGrid = new GameGrid(22, 10);
+			BlockStore = new BlockStore();
+			CurrentBlock = BlockStore.GetAndUpdate();
+		}
+
+		private bool BlockFits()
+		{
+			foreach (Position p in CurrentBlock.TilePositions())
+			{
+				if (!GameGrid.IsEmpty(p.Row, p.Column))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public void RotateBlockCW()
+		{
+			CurrentBlock.RotateClockwise();
+
+			if (!BlockFits())
+			{
+				CurrentBlock.RotateAntiClockwise();
+			}
+		}
+
+		public void RotateBlockACW()
+		{
+			CurrentBlock.RotateAntiClockwise();
+
+			if (!BlockFits())
+			{
+				CurrentBlock.RotateClockwise();
+			}
+		}
+
+		public void MoveBlockLeft()
+		{
+			CurrentBlock.Move(0, -1);
+
+			if (!BlockFits())
+			{
+				CurrentBlock.Move(0, 1);
+			}
+		}
+
+		public void MoveBlockRight()
+		{
+			CurrentBlock.Move(0, 1);
+
+			if (!BlockFits())
+			{
+				CurrentBlock.Move(0, -1);
+			}
+		}
 	}
 }
